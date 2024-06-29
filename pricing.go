2f75b483f5a16f6fe3c565a7780890bca3442b9e
web-slinger <12665151+web-slinger@users.noreply.github.com>
2024-06-29 14:01:02 +0100

    add pricing scheme and model

---
+++ pricing.go
@@ -0,0 +1,26 @@
+package main
+
+// PriceScheme contains a SKU including price and an optional special price
+type PriceScheme struct {
+	SKU          string
+	Price        int
+	SpecialPrice *SpecialPrice
+}
+
+// SpecialPrice contains the SKU, quantity and price - used for multibuy discounts
+type SpecialPrice struct {
+	SKU      string
+	Quantity int
+	Price    int
+}
+
+// PricingModel is a map of SKU to PriceScheme
+type PricingModel map[string]PriceScheme
+
+// GetPricingModel returns the pricing model
+func GetPricingModel() PricingModel {
+	// this could be built up via 2 lists
+	// list of prices keyed by SKU
+	// list of special prices keyed by SKU
+	return PricingModel{}
+}
